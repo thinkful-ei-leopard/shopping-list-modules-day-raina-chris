ecea5fabfaeed8a59e0b92659bd70f4c443639d8
Raina Shin <yewon95@vt.edu>
2020-01-13 17:30:58 -0500

    working on #5

--- scripts/store.js
+++ scripts/store.js
@@ -7,3 +7,33 @@ export default {
 };
 
 import item from './item.js';
+
+function findById(id) {
+  return store.items.find(id);
+}
+
+function addItem(name) {
+  try {
+    item.validateName(name),
+    this.items.push(item.create(name))
+  }
+  catch(error) {
+    console.log(`cannot add item: ${error.message}`);
+  }
+}
+
+function findAndToggleChecked(id) {
+  this.findById(item => item.id === id);
+  this.checked = !this.checked;
+}
+
+
+function findAndUpdateName(id, newName) {
+  try {
+    item.validateName(id),
+    findById()
+  }
+  catch(error) {
+    console.log(`cannot add item: ${error.message}`);
+  }
+}
